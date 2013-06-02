44db6c4b9c5d7e2de13f2ca2c2dbfb19767aea9e
Ramesh Rajendran <rameschinnu61@gmail.com>
2013-06-02 22:59:41 +0100

    removed unwanted files

--- target/classes/database/getAllUsers.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER // 
-	CREATE OR REPLACE PROCEDURE `getUser`(IN firstName VARCHAR(255))
-		BEGIN
-        	SELECT * 
-        		FROM User
-        		WHERE firstname = firstName;
-    	END // 
-DELIMITER ; 
